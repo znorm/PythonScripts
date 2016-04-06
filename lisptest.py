b2b67611cbff01acffbe008405fe6fcfc2479d97
znorm <sternynaomi@gmail.com>
2016-04-06 14:39:05 +0100

    add new test

--- lisptest.py
+++ lisptest.py
@@ -7,5 +7,8 @@ class TestLispNormalForms(unittest.TestCase):
 	def test_addTwoNumbers(self):
 		self.assertEqual(evaluator("(+12)"), 3)
 
+	def test_addMultipleNumbers(self):
+		self.assertEqual(evaluator("(+123456"), 21)
+
 if __name__ == '__main__':
 	unittest.main()
